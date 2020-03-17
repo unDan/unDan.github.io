16944e52a9f59608a1e23844d7f2fe2eca48a94d
Daniil Zabaluev <elgoogecaf@gmail.com>
2020-03-17 19:42:55 +0300

    Update ajax_index.php

--- php/ajax_index.php
+++ php/ajax_index.php
@@ -3,7 +3,7 @@ $email = $_POST['email'];
 $phone = $_POST['phone'];
 $date = $_POST['date'];
 
-$adress = "elgoogecaf@gmail";
+$adress = "alienterrorblade@gmail.com";
 $subject = "Запись на замер дверей";
 $message = "Имя: $name\nEmail: $email\n Номер телефона: $phone\n Дата записи: $date"; 
 mail($adress, $subject, $message, "Content-type:text/plain; charset = utf-8\r\nFrom: $email");
